a870f3b9f407d36a5f48c5e520f1bf101456161e
MateuszK <mateusz.konopka11@gmail.com>
2019-11-25 19:28:31 +0100

    Added path to desktop source file.

--- DevTime/Form1.cs
+++ DevTime/Form1.cs
@@ -8,42 +8,49 @@ namespace DevTime
 {
     public partial class Form1 : Form
     {
-        System.Timers.Timer t;
-        int h, m, s;
+        System.Timers.Timer timer;
+        int hours, minutes, seconds;
         bool timerIsOff = true;
+        string pathToSourceFile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\DevTime.csv";
+
 
         private void btnStartStop_Click(object sender, EventArgs e)
         {
             if (timerIsOff)
             {
-                t.Start();
+                timer.Start();
                 btnStartStop.BackColor = Color.Red;
                 btnStartStop.Text = "STOP";
                 timerIsOff = false;
             }
             else
             {
-                t.Stop();
+                timer.Stop();
                 btnStartStop.BackColor = Color.Lime;
                 btnStartStop.Text = "RESUME";
                 timerIsOff = true;
             }
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            t = new System.Timers.Timer();
-            t.Interval = 1000;
-            t.Elapsed += OnTimeEvent;
+            StreamWriter sw;
+
+            if (!File.Exists(pathToSourceFile))
+            {
+                sw = File.CreateText(pathToSourceFile);
+            }
+
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += OnTimeEvent;
         }
 
         private void btnSaveTime_Click(object sender, EventArgs e)
         {
-            string path = @"M:\c# stuff\HowMuchICode.csv";
-            var lineCount = File.ReadLines(path).Count() + 1;
+            var lineCount = File.ReadLines(pathToSourceFile).Count() + 1;
 
-            using (StreamWriter sw = File.AppendText(path))
+            using (StreamWriter sw = File.AppendText(pathToSourceFile))
             {
                 sw.WriteLine(lineCount + "," + DateTime.Now.ToString("MM/dd/yyyy") + "," + txtTime.Text.ToString());
             }
@@ -60,26 +67,25 @@ namespace DevTime
         {
             Invoke(new Action(() =>
                 {
-                s += 1;
-                if(s == 60)
-                {
-                    s = 0;
-                    m += 1;
-                }
-                if(m == 60)
-                {
-                    m = 0;
-                    h += 1;
-                }
-                txtTime.Text = string.Format("{0},{1},{2}", h.ToString().PadLeft(2, '0')
-                    ,m.ToString().PadLeft(2, '0'), s.ToString().PadLeft(2, '0'));
-            }));
+                    seconds += 1;
+                    if (seconds == 60)
+                    {
+                        seconds = 0;
+                        minutes += 1;
+                    }
+                    if (minutes == 60)
+                    {
+                        minutes = 0;
+                        hours += 1;
+                    }
+                    txtTime.Text = string.Format("{0},{1},{2}", hours.ToString().PadLeft(2, '0')
+                        , minutes.ToString().PadLeft(2, '0'), seconds.ToString().PadLeft(2, '0'));
+                }));
         }
 
         public Form1()
         {
             InitializeComponent();
         }
-
     }
 }
