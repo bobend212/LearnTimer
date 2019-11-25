53db641085023d71994426975d3674ce76044760
MateuszK <mateusz.konopka11@gmail.com>
2019-11-25 18:52:43 +0100

    Exit  without saving button added.

--- DevTime/Form1.Designer.cs
+++ DevTime/Form1.Designer.cs
@@ -29,12 +29,88 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.btnSaveTime = new System.Windows.Forms.Button();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.BackColor = System.Drawing.Color.Lime;
+            this.btnStartStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnStartStop.Location = new System.Drawing.Point(30, 12);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(181, 52);
+            this.btnStartStop.TabIndex = 0;
+            this.btnStartStop.Text = "START CODING!";
+            this.btnStartStop.UseVisualStyleBackColor = false;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // btnSaveTime
+            // 
+            this.btnSaveTime.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnSaveTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnSaveTime.Location = new System.Drawing.Point(30, 139);
+            this.btnSaveTime.Name = "btnSaveTime";
+            this.btnSaveTime.Size = new System.Drawing.Size(181, 52);
+            this.btnSaveTime.TabIndex = 1;
+            this.btnSaveTime.Text = "SAVE && EXIT";
+            this.btnSaveTime.UseVisualStyleBackColor = false;
+            this.btnSaveTime.Click += new System.EventHandler(this.btnSaveTime_Click);
+            // 
+            // txtTime
+            // 
+            this.txtTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtTime.Location = new System.Drawing.Point(3, 81);
+            this.txtTime.Multiline = true;
+            this.txtTime.Name = "txtTime";
+            this.txtTime.ReadOnly = true;
+            this.txtTime.Size = new System.Drawing.Size(235, 42);
+            this.txtTime.TabIndex = 2;
+            this.txtTime.Text = "00,00,00";
+            this.txtTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnExit.Location = new System.Drawing.Point(30, 197);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(181, 27);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "EXIT (without saving)";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(240, 235);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.btnSaveTime);
+            this.Controls.Add(this.btnStartStop);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DevTime!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Button btnSaveTime;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- DevTime/Form1.cs
+++ DevTime/Form1.cs
@@ -1,20 +1,85 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DevTime
 {
     public partial class Form1 : Form
     {
+        System.Timers.Timer t;
+        int h, m, s;
+        bool timerIsOff = true;
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            if (timerIsOff)
+            {
+                t.Start();
+                btnStartStop.BackColor = Color.Red;
+                btnStartStop.Text = "STOP";
+                timerIsOff = false;
+            }
+            else
+            {
+                t.Stop();
+                btnStartStop.BackColor = Color.Lime;
+                btnStartStop.Text = "RESUME";
+                timerIsOff = true;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t = new System.Timers.Timer();
+            t.Interval = 1000;
+            t.Elapsed += OnTimeEvent;
+        }
+
+        private void btnSaveTime_Click(object sender, EventArgs e)
+        {
+            string path = @"M:\c# stuff\HowMuchICode.csv";
+            var lineCount = File.ReadLines(path).Count() + 1;
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(lineCount + "," + DateTime.Now.ToString("MM/dd/yyyy") + "," + txtTime.Text.ToString());
+            }
+
+            Application.Exit();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void OnTimeEvent(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Invoke(new Action(() =>
+                {
+                s += 1;
+                if(s == 60)
+                {
+                    s = 0;
+                    m += 1;
+                }
+                if(m == 60)
+                {
+                    m = 0;
+                    h += 1;
+                }
+                txtTime.Text = string.Format("{0},{1},{2}", h.ToString().PadLeft(2, '0')
+                    ,m.ToString().PadLeft(2, '0'), s.ToString().PadLeft(2, '0'));
+            }));
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
+
     }
 }
